319c579876068db4d20d37500ee62448386164e1
siaci <siaci@DESKTOP-D94CC97>
2017-03-26 14:45:31 +0300

    updated helloworld

--- src/main/java/com/psychology/ua/HelloWorld.java
+++ src/main/java/com/psychology/ua/HelloWorld.java
@@ -2,6 +2,6 @@ package com.psychology.ua;
 
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello Anastasiia!!!");
+		System.out.println("Hello Anastasiia!!");
 	}
 }
